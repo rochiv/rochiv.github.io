a542732c035a64a9991d9a896715c16075a05a5a
Rohit Chivukula <rohitchivukula9@gmail.com>
2025-04-01 20:56:08 -0400

    Update asset manifest and build files: replace main CSS and JS with chunked versions, add service worker and precache manifest, and remove old files. Enhance index.html to reference new assets and improve loading performance.

---
+++ build/precache-manifest.bec2431b44d890916ee00fc6794a3dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac55729aa8781da0b942f436ed75fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3561a225181b429dd057",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49e013851094a3841bd3",
+    "url": "/static/js/2.504c7a01.chunk.js"
+  },
+  {
+    "revision": "3561a225181b429dd057",
+    "url": "/static/js/main.ed36a3ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
